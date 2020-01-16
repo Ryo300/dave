cddfce8df2fc1714a42794e3700fd362efd4afd0
ArcaneDave <rdtedja@gmail.com>
2020-01-16 08:00:23 +0700

    Change in Java

--- hello.java
+++ hello.java
@@ -1,4 +1,6 @@
 public class Hello{
 	public statis void main(String[] args}{
-			System.out.println("Hello gan");
-}}
+			System.out.println("Hello gan!");
+			System.out.println("This is a change to be commited");
+	}
+}
